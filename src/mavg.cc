9d390a0f6d6cc7ea92b7fb08b73a76e6972a13bd
Altertoriel <79248736+system32unknown@users.noreply.github.com>
2026-07-31 02:31:23 +0900

    fuck 2

--- src/mavg.cc
+++ src/mavg.cc
@@ -26,7 +26,7 @@ mavg::solve_electronics()
     this->value = f * this->value + (1.f - f)*v;
 
     float clamped_value = 0.f;
-    if (settings["disable_overloader"]->v.b) tclampf(this->value, 0.f, 1.f)
+    if (settings["disable_overloader"]->v.b) clamped_value = tclampf(this->value, 0.f, 1.f);
     else clamped_value = this->value;
     this->s_out[0].write(clamped_value);
     return 0;
