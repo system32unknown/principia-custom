dbb154b7b60c86f6aca38de702b435d306b3cbfc
Altertoriel <79248736+system32unknown@users.noreply.github.com>
2026-08-20 07:21:57 +0900

    Remove 'utf8' from Lua function registration
    Removed 'utf8' from the list of registered Lua functions.

--- src/escript.cc
+++ src/escript.cc
@@ -310,7 +310,7 @@ static const char* ignore[] = {
     "tonumber", "tostring", "type", "unpack", "wl", "xpcall", "string",
     "_", "set_textdomain", "get_build_id", "ngettext", "_ENV",
     "game", "this", "socket", "step", "world", "cam", "_G",
-    "init", "include", "world.___persist_entity", "bit32", "utf8", 0
+    "init", "include", "world.___persist_entity", "bit32", 0
 };
 
 static void
